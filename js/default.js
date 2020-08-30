c61a30c29b5003ae54a2b1ede4b29070f95c6d7a
joohoonkim <joohoon.kim@outlook.com>
2020-08-30 16:09:03 -0400

    static overflow change.

--- js/default.js
+++ js/default.js
@@ -24,22 +24,16 @@ function checkOverflow(){
     if (document.getElementById("intro-section").offsetHeight < document.getElementById("intro-section").scrollHeight ||
     document.getElementById("intro-section").offsetWidth < document.getElementById("intro-section").scrollWidth){
         document.getElementById("intro-section").setAttribute("style","height:100%");
-    } else {
-        document.getElementById("intro-section").setAttribute("style","height:100vh");
     }
 
     if (document.getElementById("exp-section").offsetHeight < document.getElementById("exp-section").scrollHeight ||
     document.getElementById("exp-section").offsetWidth < document.getElementById("exp-section").scrollWidth){
         document.getElementById("exp-section").setAttribute("style","height:100%");
-    } else {
-        document.getElementById("exp-section").setAttribute("style","height:100vh");
     }
     
     if (document.getElementById("proj-section").offsetHeight < document.getElementById("proj-section").scrollHeight ||
     document.getElementById("proj-section").offsetWidth < document.getElementById("proj-section").scrollWidth){
         document.getElementById("proj-section").setAttribute("style","height:100%");
-    } else {
-        document.getElementById("proj-section").setAttribute("style","height:100vh");
     }
 }
 
